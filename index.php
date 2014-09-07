e423d6fdeff6287cc6933f9bcd9b79676d9e360f
Brian Travis <btrav528@gmail.com>
2014-09-07 14:22:37 -0400

    Cache layer done.

--- index.php
+++ index.php
@@ -9,18 +9,50 @@ function GetConnection() {
 
 if (!empty($_POST['year'])) {
 	
+	$sql = ("SELECT * FROM `top250` WHERE Year=" . $_POST['year']);
+	$filename=md5($sql);
+	if(file_exists ( $filename)){
+		$result=file_get_contents($filename);
+		$out = explode("<!-- E -->", $result);
+		//while($count<=9||$result!=false){
+		//$result=unserialize($result);
+		//var_dump($out);
+		$count=0;
+		echo "<table class='table' border='1'><th>Rank</th><th>Title</th><th>Year</th><th>Number of votes</th><th>Rating</th>";
+		while($count<count($out)-1){
+			$row=unserialize($out[$count]);
+			echo "<tr><td>" . $row['Rank'] . "</td><td>" . $row['Title'] . "</td><td>" . $row['Year'] . "</td><td>" . $row['number_of_votes'] . "</td><td>" . $row['Rating'] . "</td></tr>";
+
+		$count++;
+		}
+		}
+		
+		
+	else{
 
 	$conn = GetConnection();
-	$sql = ("SELECT * FROM `top250` WHERE Year=" . $_POST['year']);
+	
 	$result = mysqli_query($conn, $sql);
-	echo "<table class='table' border='1'><th>Rank</th><th>Title</th><th>Year</th><th>Number of votes</th><th>Rating</th>";
 
+	echo "<table class='table' border='1'><th>Rank</th><th>Title</th><th>Year</th><th>Number of votes</th><th>Rating</th>";
+	
+	
+	$cacheFilename = md5($sql);
+	
+	
+	$data=array();
 	while ($row = mysqli_fetch_array($result)) {
+		array_merge ($row, $data);
+		var_dump($data);
+		$serializedData = serialize($row);
+		file_put_contents($cacheFilename, $serializedData."<!-- E -->", FILE_APPEND);
 		echo "<tr><td>" . $row['Rank'] . "</td><td>" . $row['Title'] . "</td><td>" . $row['Year'] . "</td><td>" . $row['number_of_votes'] . "</td><td>" . $row['Rating'] . "</td></tr>";
 
 	}
 
-		//mysqli_close($conn);
+
+		mysqli_close($conn);
+		}
 	}
 else {
 	$conn = GetConnection();
